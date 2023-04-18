0e32e97e550aa2562762d49f469fc2a3fb54aa26
Tiago Moraes <tiagotlm@live.com>
2023-04-18 19:10:27 -0300

    Update ManipularTexto.py

--- ManipularTexto.py
+++ ManipularTexto.py
@@ -1,1 +1,8 @@
-#Apenas um teste de tratamento de texto
+# Apenas um teste de tratamento de texto
+
+frase = input('Insira uma frase para o teste: ')
+# frase[9:21:2] [começa no caracter 9: vai até o 21: pulando de 2 em 2]
+# print('A nona string é o: {}'.format(frase[9]))
+# Contando quantas letras O apareceram dentro do intervalo
+# print(frase.count('o',0,20))
+print(frase.replace('Tiago', 'Camila'))
